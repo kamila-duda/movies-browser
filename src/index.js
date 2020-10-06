7189873f76a4a368f39238c202a1ba50820a307d
const.kamil.lazarski@gmail.com <const.kamil.lazarski@gmail.com>
2020-10-06 23:22:44 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import store from "./app/store";
-import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
-import { GlobalStyle } from "./GlobalStyle";
+import { GlobalStyle } from "theme/GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
+import { theme } from "theme/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
       </ThemeProvider>
-    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
