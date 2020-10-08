e6d29afee4aafff80de8237e5a83c50e6b511cab
Michal Janiec <michal.janiec95@gmail.com>
2020-10-08 22:41:25 +0200

    Created SearchingErrorPage according to project in Figma.

---
+++ src/common/SearchingErrorPage/styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    margin-top:40px;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+`;
+
+export const HeaderTitle = styled.h1`
+    margin:0px 0px 40px 0px;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 43px;
+    letter-spacing: 0px;
+    align-self:flex-start;
+`;
+
+export const StyledImage = styled.img`
+    max-width:100%;
+    margin:0px auto;
+`;
