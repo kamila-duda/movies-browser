6bfca5dceaee678e8a133299e747d157e56ed089
Kamila Duda <kakus86@o2.pl>
2020-10-08 09:16:23 +0200

    Component Container - added padding after first review

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const StyledContainer = styled.main`
   max-width: 1350px;
-  margin: 40px auto;
+  padding-top: 90px;
+  margin: auto;
   @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
-    margin: 10px;
+    padding-top: 140px;
   }
 `;
