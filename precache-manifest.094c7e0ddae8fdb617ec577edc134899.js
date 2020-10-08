e6204c4fd5e5210b4814c522806383e93c288e2a
Kamila Duda <kakus86@o2.pl>
2020-10-08 10:27:51 +0200

    Updates

---
+++ precache-manifest.094c7e0ddae8fdb617ec577edc134899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af7e96af8ff6fb638941c17d1685e64",
+    "url": "/movies-browser/index.html"
+  },
+  {
+    "revision": "a06302dfb5ef771ebb71",
+    "url": "/movies-browser/static/js/2.f822d4e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-browser/static/js/2.f822d4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98696a3908ffa593f49e",
+    "url": "/movies-browser/static/js/main.674acdbd.chunk.js"
+  },
+  {
+    "revision": "ed7c825b9a2b890dc49f",
+    "url": "/movies-browser/static/js/runtime-main.fe7d22ad.js"
+  },
+  {
+    "revision": "56dd92fe52ec8fc3a08c48956b883f60",
+    "url": "/movies-browser/static/media/Search.56dd92fe.svg"
+  },
+  {
+    "revision": "cdf24f103dd27027831a744b87c4311d",
+    "url": "/movies-browser/static/media/Video.cdf24f10.svg"
+  }
+]);
