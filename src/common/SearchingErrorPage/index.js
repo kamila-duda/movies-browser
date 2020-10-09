106450eb9e9062df982cf7d4241ad5f226871463
JaniecMichal <67117780+JaniecMichal@users.noreply.github.com>
2020-10-09 09:48:42 +0200

    Created SearchingErrorPage according to project in Figma. (#5)

---
+++ src/common/SearchingErrorPage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import searchErrorImage from "./image/Picture.svg"
+import { Wrapper, HeaderTitle, StyledImage } from "./styled"
+
+const SearchingErrorPage = () => {
+
+    return (
+        <Wrapper>
+            <HeaderTitle>Sorry, there are no results for "Muan"</HeaderTitle>
+            <StyledImage src={searchErrorImage} alt="Searching Error" />
+        </Wrapper>
+    )
+
+};
+
+export default SearchingErrorPage;
