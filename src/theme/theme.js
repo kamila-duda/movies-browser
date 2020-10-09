db03074930419caa3c0944c59b2e7f8744edd82f
Michal Janiec <michal.janiec95@gmail.com>
2020-10-10 00:47:22 +0200

    Added colors which are needed for Pagination component

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,12 +1,16 @@
 export const theme = {
     color: {
-     white: "#FFFFFF",
-     mercury: "#E5E5E5",//background
-     mystic: "#E4E6F0",//szare tło tagów
-     waterloo: "#7E839A",//pasek search, szary opis w kafelkach
-     stormGray: "#74788B",//szary opis w dużym kaflu
-     scienceBlue: "0044CC",//button w error
-     black: "#000000",
+        white: "#FFFFFF",
+        mercury: "#E5E5E5",//background
+        mystic: "#E4E6F0",//szare tło tagów
+        waterloo: "#7E839A",//pasek search, szary opis w kafelkach
+        gray: "#E4E6F0",//nieaktywne buttony paginacji
+        stormGray: "#74788B",//szary opis w dużym kaflu
+        darkedGray: "#7E839A",
+        lightBlue: "#D6E4FF", // aktywne buttony paginacji
+        blue: "#0044CC",
+        scienceBlue: "0044CC",//button w error
+        black: "#000000"
     },
     breakpoint: {
         extraSmall: "575.98px",
@@ -14,4 +18,4 @@ export const theme = {
         medium: '991.98px',
         large: "1199.98px",
     }
- };
+};
