5ae1f03debcd1ce3ad807be058d0c094b239d2d1
const.kamil.lazarski@gmail.com <const.kamil.lazarski@gmail.com>
2020-10-13 23:34:20 +0200

    after last merged

--- src/common/Tiles/styled.js
+++ src/common/Tiles/styled.js
@@ -1,12 +1,12 @@
 import styled, { css } from "styled-components";
 
 export const StyledTilesContainer = styled.div`
-    display: grid;
-    grid-gap: 24px; 
-    grid-template-columns: repeat(4, minmax(324px, 1fr));
-    justify-content: center;
-    height: auto;
-    margin-top: 214px;
+  display: grid;
+  grid-gap: 24px; 
+  grid-template-columns: repeat(4, minmax(324px, 1fr));
+  justify-content: center;
+  height: auto;
+  margin-top: 214px;
 
     ${({ horizontal }) =>
     horizontal &&
