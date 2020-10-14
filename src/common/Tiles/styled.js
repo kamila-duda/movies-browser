2c98a25ca915709e6b894f2612512c648239d4aa
Michal Janiec <michal.janiec95@gmail.com>
2020-10-14 23:39:39 +0200

    Changed grid-gap to fit into container.

--- src/common/Tiles/styled.js
+++ src/common/Tiles/styled.js
@@ -17,6 +17,7 @@ export const StyledTilesContainer = styled.div`
     people &&
     css`
       grid-template-columns: repeat(6, minmax(208px, 1fr));
+      grid-gap: 20px;
       @media (max-width: ${({ theme }) => theme.media.tablet}) {
         grid-template-columns: repeat(4, 1fr);
       }
