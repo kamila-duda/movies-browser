421b50ed09d702c7fff49bcde114374155463ab3
Michal Janiec <michal.janiec95@gmail.com>
2020-10-17 00:20:59 +0200

    Added states: configurationParameters with configuration file for APi; genres which are downloaded from API. Added new reducers/action for new added states. Changed fetchPopularMovies reducer into fetchData (for more universality). Exported reducers and selectors for new states.

--- src/features/moviesSlice.js
+++ src/features/moviesSlice.js
@@ -3,10 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const moviesSlice = createSlice({
   name: `movies`,
   initialState: {
+    configurationParameters: {},
     movies: {},
+    genres: [],
   },
   reducers: {
-    fetchPopularMovies: () => { },
+    fetchData: () => { },
+    setConfigurationParameters: (state, { payload: basicParameters }) => {
+      state.configurationParameters = basicParameters;
+    },
+    setGenres: (state, { payload: genres }) => {
+      state.genres = genres;
+    },
     setMovies: (state, { payload: popularMovies }) => {
       state.movies = popularMovies;
     },
@@ -14,12 +22,16 @@ const moviesSlice = createSlice({
 });
 
 export const {
-  fetchPopularMovies,
-  setMovies
+  fetchData,
+  setConfigurationParameters,
+  setGenres,
+  setMovies,
 } = moviesSlice.actions;
 
 export const selectMovies = state => state.movies;
 export const selectResults = state => selectMovies(state).movies.results;
+export const selectGenres = state => selectMovies(state).genres.genres;
+export const selectConfigurationParameters = state => selectMovies(state).configurationParameters.images;
 
 export default moviesSlice.reducer;
 
