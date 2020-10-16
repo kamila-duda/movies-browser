faa211726f3d3bab7d3b3cdb673cd7de1c3da53b
Michal Janiec <michal.janiec95@gmail.com>
2020-10-17 00:18:12 +0200

    Deleted not necessary elements like useEffect. Added protecion to the component if tags are empty.

--- src/common/Tiles/Tile/TileTags/index.js
+++ src/common/Tiles/Tile/TileTags/index.js
@@ -1,19 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { StyledTileTag, StyledTileTags } from "./styled";
 import { selectGenres } from "features/moviesSlice";
 import { useSelector } from "react-redux";
 
 const TileTags = ({ tags }) => {
   const genres = useSelector(selectGenres);
-  return (
-    <StyledTileTags>
-      {tags.map((tag) => (
-        <StyledTileTag key={tag}>
-          {genres.find(({ id }) => id === tag).name}
-        </StyledTileTag>
-      ))}
-    </StyledTileTags>
-  );
+  if (tags.length > 0) {
+    return (
+      <StyledTileTags>
+        {tags.map((tag) => (
+          <StyledTileTag key={tag}>
+            {genres.find(({ id }) => id === tag).name}
+          </StyledTileTag>
+        ))}
+      </StyledTileTags>
+    );
+  } return "";
 };
 
 export default TileTags;
