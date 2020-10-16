781613ead6b8d7eef537c56b9d2be91cd4080d33
Michal Janiec <michal.janiec95@gmail.com>
2020-10-17 01:19:46 +0200

    Add function which setLoading with true or false.

--- src/features/moviesSaga.js
+++ src/features/moviesSaga.js
@@ -1,21 +1,24 @@
-import { takeEvery, call, put } from "redux-saga/effects";
-import { fetchData, setConfigurationParameters, setGenres, setMovies } from "features/moviesSlice";
+import { takeEvery, call, put, delay } from "redux-saga/effects";
+import { fetchData, setConfigurationParameters, setGenres, setMovies, setLoading, setError } from "features/moviesSlice";
 import { getDataApi } from "./getDataApi";
 
 function* fetchDataMoviesHandler() {
   try {
-    const configurations = yield call(getDataApi, "configurationData")
+    yield put(setLoading(true));
+    yield delay(1500);
+    const configurations = yield call(getDataApi, "configurationData");
     yield put(setConfigurationParameters(configurations));
-    const genresList = yield call(getDataApi, "genresData")
+    const genresList = yield call(getDataApi, "genresData");
     yield put(setGenres(genresList));
-    const popularMovies = yield call(getDataApi, "popularMoviesData")
+    const popularMovies = yield call(getDataApi, "popularMoviesData");
     yield put(setMovies(popularMovies));
   } catch (error) {
     yield call(alert, "Coś poszło nie tak!");
-  }
+  };
+  yield put(setLoading(false));
 }
 
 export function* watchFetchingAction() {
   yield takeEvery(fetchData.type, fetchDataMoviesHandler);
 
-}
+};
