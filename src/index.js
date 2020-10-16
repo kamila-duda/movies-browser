641e566a61a3e6397feea3ab933db5feda75e5b7
Michal Janiec <michal.janiec95@gmail.com>
2020-10-16 22:07:23 +0200

    Added Provider for Store for Redux.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { GlobalStyle } from "theme/GlobalStyle";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 import { theme } from "theme/theme";
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-          <App />
+        <App />
       </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
