bf3cc281a0584f7b52857b3780081f7943575428
Michal Janiec <michal.janiec95@gmail.com>
2020-10-16 22:10:58 +0200

    Adjusted function to download API data to API key, language and page parameters.

--- src/features/MovieListPage/getPopularMovies.js
+++ src/features/MovieListPage/getPopularMovies.js
@@ -1,9 +1,15 @@
-export const getPopularMovies= async () => {
-    const response = await fetch("/movies-browser/popularMovies.json");
-  
-    if (!response.ok) {
-      new Error(response.statusText);
-    }
-  
-    return await response.json();
-  };
+import { apiKey } from "features/apiKey";
+const language = "en-US";
+const page = 1;
+
+export const getPopularMovies = async () => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=${language}&page=${page}`
+  );
+
+  if (!response.ok) {
+    new Error(response.statusText);
+  }
+
+  return await response.json();
+};
