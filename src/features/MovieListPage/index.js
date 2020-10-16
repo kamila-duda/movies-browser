17cd22aebdef63c37ad997cc78765f69633696ba
Michal Janiec <michal.janiec95@gmail.com>
2020-10-16 22:24:23 +0200

    Added dynamic downloaded PopularMovies from API.

--- src/features/MovieListPage/index.js
+++ src/features/MovieListPage/index.js
@@ -1,39 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Container from "common/Container";
 import Tiles from "common/Tiles";
 import Tile from "common/Tiles/Tile";
-import React, { useEffect, useState } from "react";
-import moviePoster from "assets/images/jpg/poster.jpg";
-import starIcon from "assets/images/svg/star.svg";
 import Pagination from "common/Pagination";
+import { fetchPopularMovies, selectResults } from "features/moviesSlice";
 //import { getPopularMovies } from "./getPopularMovies";
 
 const MovieListPage = () => {
+  const popularMovies = useSelector(selectResults);
+  console.log(popularMovies);
+  const dispatch = useDispatch();
   const baseURL = "http://image.tmdb.org/t/p/w500";
 
-  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch("/movies-browser/popularMovies.json")
-      .then((response) => response.json())
-      .then((response) => setMovies(response.results))
-      .catch((error) => console.error(error));
+    dispatch(fetchPopularMovies())
   }, []);
-  // const movie = {
-  //   poster: moviePoster,
-  //   header: "Mulan",
-  //   subheader: "2020",
-  //   tags: ["film", "film"],
-  //   review: starIcon,
-  //   description:
-  //     "A young Chinese maiden disguises herself as a male warrior in order to save her father",
-  // };
-  //console.log(movies[9])
-  return (
-    <Container>
-      <Tiles
-        title="Popular movies"
-        body={movies.map((movie) => (
+
+  const generatePopularMoviesList = () => {
+    if (popularMovies) {
+      return (
+        popularMovies.map((movie) => (
           <Tile
-          key={movie.id}
+            key={movie.id}
             poster={`${baseURL}${movie.poster_path}`}
             header={movie.title}
             subheader={movie.release_date}
@@ -41,7 +30,16 @@ const MovieListPage = () => {
             voteAverage={movie.vote_average}
             review={movie.vote_count}
           />
-        ))}
+        )))
+    }
+    return "";
+  };
+
+  return (
+    <Container>
+      <Tiles
+        title="Popular movies"
+        body={generatePopularMoviesList()}
       />
       <Pagination />
     </Container>
