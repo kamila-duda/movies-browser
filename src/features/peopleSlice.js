88d76347ee1555b54d8624e6c34905e030ef4877
Michal Janiec <michal.janiec95@gmail.com>
2020-10-19 22:51:16 +0200

    Added actions to changed currentPage state to use this actions by Pagination. Changed fetchPopularPeopleSucess to fetch all response from API. Changed selector for popularPeopole according to previous writed changes.

--- src/features/peopleSlice.js
+++ src/features/peopleSlice.js
@@ -13,28 +13,42 @@ export const peopleSlice = createSlice({
       state.loading = true;
     },
     fetchPopularPeopleSuccess: (state, { payload: people }) => {
-      state.people = people.results;
+      state.people = people;
       state.loading = false;
     },
     fetchPopularPeopleError: (state) => {
       state.people = [];
       state.loading = false;
     },
-    setCurrentPage: (state, { payload: people }) => {
-      state.currentPage = people.page;
+    increaseCurrentPage: (state) => {
+      state.currentPage = ++state.currentPage;
     },
-  },
+    decreaseCurrentPage: (state) => {
+      state.currentPage = --state.currentPage;
+    },
+    setCurrentPageFirst: (state) => {
+      state.currentPage = 1;
+    },
+    setCurrentPageLast: (state, { payload: lastPage }) => {
+      state.currentPage = lastPage;
+    },
+  }
 });
 
 export const {
   fetchPopularPeople,
   fetchPopularPeopleSuccess,
   fetchPopularPeopleError,
-  setCurrentPage,
+  increaseCurrentPage,
+  decreaseCurrentPage,
+  setCurrentPageFirst,
+  setCurrentPageLast
 } = peopleSlice.actions;
 
 const selectPeopleState = (state) => state.people;
-export const selectPeople = (state) => selectPeopleState(state).people;
+export const selectPeople = (state) => selectPeopleState(state).people.results;
 export const selectLoading = (state) => selectPeopleState(state).loading;
+export const selectTotalPages = (state) => selectPeopleState(state).people.total_pages;
+export const selectCurrentPage = (state) => selectPeopleState(state).currentPage;
 
 export default peopleSlice.reducer;
