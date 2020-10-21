ceb56c7553e078da73b8c662a2781f5e9741ca66
Michal Janiec <michal.janiec95@gmail.com>
2020-10-21 23:57:36 +0200

    Added isError state, actions and selectors for this state. Removing not necessary actions.

--- src/features/peopleSlice.js
+++ src/features/peopleSlice.js
@@ -5,6 +5,7 @@ export const peopleSlice = createSlice({
   initialState: {
     people: [],
     loading: true,
+    isError: false,
     currentPage: 1,
     person: [],
     personId: null,
@@ -14,14 +15,12 @@ export const peopleSlice = createSlice({
   reducers: {
     fetchPopularPeople: (state) => {
       state.loading = true;
+      state.isError = false;
     },
     fetchPopularPeopleSuccess: (state, { payload: people }) => {
       state.people = people;
       state.loading = false;
-    },
-    fetchPopularPeopleError: (state) => {
-      state.people = [];
-      state.loading = false;
+      state.isError = false;
     },
     increaseCurrentPage: (state) => {
       state.currentPage = ++state.currentPage;
@@ -47,7 +46,8 @@ export const peopleSlice = createSlice({
         state.personCrew = details.movie_credits.crew;
       }
     },
-    fetchPersonDetailsError: (state) => {
+    setError: (state) => {
+      state.isError = true;
       state.loading = false;
     },
   },
@@ -56,20 +56,20 @@ export const peopleSlice = createSlice({
 export const {
   fetchPopularPeople,
   fetchPopularPeopleSuccess,
-  fetchPopularPeopleError,
   increaseCurrentPage,
   decreaseCurrentPage,
   setCurrentPageFirst,
   setCurrentPageLast,
   fetchPersonDetails,
   fetchPersonDetailsSuccess,
-  fetchPersonDetailsError,
   setCurrentPage,
+  setError,
 } = peopleSlice.actions;
 
 const selectPeopleState = (state) => state.people;
 export const selectPeople = (state) => selectPeopleState(state).people.results;
 export const selectLoading = (state) => selectPeopleState(state).loading;
+export const selectIsError = (state) => selectPeopleState(state).isError;
 export const selectTotalPages = (state) =>
   selectPeopleState(state).people.total_pages;
 export const selectCurrentPage = (state) =>
