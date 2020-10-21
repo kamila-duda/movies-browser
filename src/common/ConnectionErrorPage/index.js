321fc9753c956b33b873f2b93e85b30cfca71c45
Michal Janiec <michal.janiec95@gmail.com>
2020-10-21 23:36:24 +0200

    Added actions for redirect button which depend from features which is active (can be movies or people).

--- src/common/ConnectionErrorPage/index.js
+++ src/common/ConnectionErrorPage/index.js
@@ -1,14 +1,36 @@
+
 import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { toMoviesList, toPeopleList } from "routes";
 import Picture from "./image/Danger.svg"
 import { StyledImage, ErrorTitle, Wrapper, ErrorMessage, HomePageButton } from "./styled";
+import { fetchPopularMovies } from "features/moviesSlice";
+import { fetchPopularPeople } from "features/peopleSlice";
 
-const ConnectionErrorPage = () => {
+const ConnectionErrorPage = ({ pageType }) => {
+    const dispatch = useDispatch();
+    console.log(pageType);
     return (
         <Wrapper>
             <StyledImage src={Picture} alt="ConnectionErrorImage" />
             <ErrorTitle>Ooops! Something went wrong...</ErrorTitle>
             <ErrorMessage>Please check your network connection and try again</ErrorMessage>
-            <HomePageButton>Back to home page</HomePageButton>
+            {pageType === "movies" ? (
+                <Link
+                    to={toMoviesList()}
+                    onClick={() => dispatch(fetchPopularMovies())}
+                >
+                    <HomePageButton>Back to home page</HomePageButton>
+                </Link>
+            ) : (
+                    <Link
+                        to={toPeopleList()}
+                        onClick={() => dispatch(fetchPopularPeople())}
+                    >
+                        <HomePageButton>Back to home page</HomePageButton>
+                    </Link>
+                )}
         </Wrapper>
     )
 };
