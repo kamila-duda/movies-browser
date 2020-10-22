803c2f7e02332be5038f3304926a8a49849f4c23
kamila-duda <kakus86@o2.pl>
2020-10-22 13:40:42 +0200

    added props to SearchingErrorPage component

--- src/common/SearchingErrorPage/index.js
+++ src/common/SearchingErrorPage/index.js
@@ -3,11 +3,11 @@ import searchErrorImage from "assets/images/svg/Picture.svg"
 
 import { Wrapper, HeaderTitle, StyledImage } from "./styled"
 
-const SearchingErrorPage = () => {
+const SearchingErrorPage = ({query}) => {
 
     return (
         <Wrapper>
-            <HeaderTitle>Sorry, there are no results for "Muan"</HeaderTitle>
+            <HeaderTitle>Sorry, there are no results for "{query}"</HeaderTitle>
             <StyledImage src={searchErrorImage} alt="Searching Error" />
         </Wrapper>
     )
