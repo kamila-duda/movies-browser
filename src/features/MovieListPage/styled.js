2bf46c5d973a90335f67ec1da02b5bd751d75434
kamila-duda <kakus86@o2.pl>
2020-10-22 12:23:45 +0200

    changed display in movie's tile

--- src/features/MovieListPage/styled.js
+++ src/features/MovieListPage/styled.js
@@ -3,6 +3,6 @@ import { Link } from 'react-router-dom';
 
 export const StyledLink = styled(Link)`
   text-decoration:none;
-  display:block;
+  display: flex;
   color:${({ theme }) => theme.color.black};
 `;
