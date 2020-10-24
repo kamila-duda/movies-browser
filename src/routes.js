d372ac0b5cc65e3fe1206d705168d671b9dadcb8
Michal Janiec <michal.janiec95@gmail.com>
2020-10-25 00:54:13 +0200

    added new routes which depend from the page

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 export const toMoviesList = () => "/movies";
-export const toMovieDetails = ({id} = {id: ":id"}) => `/movies/${id}`;
+export const toMoviesNextPage = ({ page } = { page: 1 }) => `/movies/page=${page}`;
+export const toMovieDetails = ({ id } = { id: ":id" }) => `/movies/${id}`;
 export const toPeopleList = () => "/people";
-export const toPersonDetails = ({id} = {id: ":id"}) => `/people/${id}`;
+export const toPersonDetails = ({ id } = { id: ":id" }) => `/people/${id}`;
