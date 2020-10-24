3e5f1b9c1ce107c55528fcc3b5d77c2108997d5b
Michal Janiec <michal.janiec95@gmail.com>
2020-10-24 21:47:57 +0200

    Fixed searching if currentPage is bigger than 1 for Movies and People

--- src/features/Search/index.js
+++ src/features/Search/index.js
@@ -1,20 +1,27 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import search from "assets/images/svg/Search.svg";
 import { key } from "./searchQueryParameter";
 import { useQueryParameter, useReplaceQueryParameter } from "./queryParameter";
+import { setCurrentPageFirst as setPageFirstMovies } from "features/moviesSlice";
+import { setCurrentPageFirst as setPageFirstPeople } from "features/peopleSlice";
 import { StyledIcon, StyledInput, StyledLabel } from "./styled";
-import { useLocation } from "react-router-dom";
+
 
 const Search = () => {
   const query = useQueryParameter(key);
   const replaceQueryParameter = useReplaceQueryParameter();
   const location = useLocation();
+  const dispatch = useDispatch();
   const searchMovies = location.pathname.includes("movies");
   const onInputChange = ({ target }) => {
     replaceQueryParameter({
       key: key,
       value: target.value.trim() !== "" ? target.value : "",
     });
+    if (searchMovies) dispatch(setPageFirstMovies());
+    dispatch(setPageFirstPeople());
   };
   return (
     <StyledLabel>
