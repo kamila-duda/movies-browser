4245fe8185ac27c9cc19a97ae522fdbf8b73090d
Michal Janiec <michal.janiec95@gmail.com>
2020-10-28 15:24:07 +0100

    Deleted not necessary route

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 export const toMoviesList = () => "/movies";
-export const toMoviesNextPage = ({ page } = { page: 1 }) => `/movies/page=${page}`;
 export const toMovieDetails = ({ id } = { id: ":id" }) => `/movies/${id}`;
 export const toPeopleList = () => "/people";
 export const toPersonDetails = ({ id } = { id: ":id" }) => `/people/${id}`;
