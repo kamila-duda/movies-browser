e7d1975c134f208888795a18d876be5e5d9d0e2d
kamila-duda <kakus86@o2.pl>
2020-10-30 23:10:35 +0100

    added FavoriteMovies Component

--- src/features/MovieListPage/index.js
+++ src/features/MovieListPage/index.js
@@ -5,7 +5,6 @@ import Tiles from "common/Tiles";
 import Tile from "common/Tiles/Tile";
 import Pagination from "common/Pagination";
 import {
-  fetchMovieDetails,
   fetchPopularMovies,
   selectCurrentPage,
   selectTotalPages,
@@ -17,16 +16,19 @@ import {
   setCurrentPageFirst,
   setCurrentPageLast,
   selectResults,
+  toggleFavoriteMovies,
+  selectFavoriteMovie,
 } from "features/moviesSlice";
 import nonePoster from "assets/images/png/nonePoster.png";
 import Spinner from "features/Spinner";
-import { StyledLink } from "./styled";
-import { toMovieDetails } from "routes";
+import { StyledDiv, StyledFontAwesomeIcon } from "./styled";
 import { useQueryParameter } from "hooks/useQueryParameter";
 import { key } from "features/Search/searchQueryParameter";
 import ConnectionErrorPage from "common/ConnectionErrorPage";
 import SearchingErrorPage from "common/SearchingErrorPage";
 import UpButton from "common/UpButton";
+import { faHeart as fasFaHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as farFaHeart } from "@fortawesome/free-regular-svg-icons";
 
 const MovieListPage = () => {
   const query = useQueryParameter(key);
@@ -49,66 +51,77 @@ const MovieListPage = () => {
     }
   }, [dispatch, currentPage, page, query]);
 
+  const favoriteMovie = useSelector(selectFavoriteMovie);
+
   if (isError) {
     return (
       <Container>
         <ConnectionErrorPage pageType="movies" />
       </Container>
     );
-  };
+  }
   if (results === 0 && query) {
     return (
       <Container>
         <SearchingErrorPage query={query} />
       </Container>
     );
-  };
+  }
   return (
     <Container>
-      <UpButton/>
-      {loading ? (query? (<Tiles title={`Search results for ${query}`} body={<Spinner />} />):
-        <Tiles title="Search results for popular movies" body={<Spinner />} />
+      <UpButton />
+      {loading ? (
+        query ? (
+          <Tiles title={`Search results for ${query}`} body={<Spinner />} />
+        ) : (
+          <Tiles title="Search results for popular movies" body={<Spinner />} />
+        )
       ) : (
-          <Tiles
-            title={query ? `Search results for "${query}" (${results})` : title}
-            body={movies.map((movie) => (
-              <StyledLink
+        <Tiles
+          title={query ? `Search results for "${query}" (${results})` : title}
+          body={movies.map((movie) => (
+            <StyledDiv>
+              <Tile
+                movieId={movie.id}
                 key={movie.id}
-                to={toMovieDetails({ id: movie.id })}
-                onClick={() => dispatch(fetchMovieDetails(movie.id))}
-              >
-                <Tile
-                  key={movie.id}
-                  poster={
-                    movie.poster_path === null
-                      ? nonePoster
-                      : `${images}${posterSize}${movie.poster_path}`
-                  }
-                  header={movie.title}
-                  subheader={
-                    movie.release_date ? movie.release_date.substring(0, 4) : ""
-                  }
-                  tags={movie.genre_ids}
-                  voteAverage={movie.vote_average}
-                  review={movie.vote_count}
-                />
-              </StyledLink>
-            ))}
-          />
-        )}
+                poster={
+                  movie.poster_path === null
+                    ? nonePoster
+                    : `${images}${posterSize}${movie.poster_path}`
+                }
+                header={movie.title}
+                subheader={
+                  movie.release_date ? movie.release_date.substring(0, 4) : ""
+                }
+                tags={movie.genre_ids}
+                voteAverage={movie.vote_average}
+                review={movie.vote_count}
+              />
+              <StyledFontAwesomeIcon
+                onClick={() => dispatch(toggleFavoriteMovies(movie))}
+                icon={
+                  favoriteMovie.find((fav) => fav.id === movie.id)
+                    ? fasFaHeart
+                    : farFaHeart
+                }
+              />
+            </StyledDiv>
+          ))}
+        />
+      )}
 
       {loading ? (
         ""
       ) : (
-          <Pagination
-            currentPage={currentPage}
-            lastPage={lastPage}
-            setCurrentPageFirst={setCurrentPageFirst}
-            decreaseCurrentPage={decreaseCurrentPage}
-            increaseCurrentPage={increaseCurrentPage}
-            setCurrentPageLast={setCurrentPageLast}
-          />
-        )}
+        <Pagination
+          currentPage={currentPage}
+          lastPage={lastPage}
+          setCurrentPageFirst={setCurrentPageFirst}
+          decreaseCurrentPage={decreaseCurrentPage}
+          increaseCurrentPage={increaseCurrentPage}
+          setCurrentPageLast={setCurrentPageLast}
+        />
+      )}
     </Container>
   );
 };
