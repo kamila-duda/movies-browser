977643cae738c4e7771ecdedda1cd978611a5f71
Michal Janiec <michal.janiec95@gmail.com>
2020-11-30 22:53:07 +0100

    Renamed path for saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,5 +1,5 @@
-import { watchFetchPopularMovies } from "features/popularMoviesSaga";
-import { watchFetchPopularPeople } from "features/popularPeopleSaga";
+import { watchFetchPopularMovies } from "features/MoviesSaga";
+import { watchFetchPopularPeople } from "features/PeopleSaga";
 import { all } from "redux-saga/effects";
 
 export default function* rootSaga() {
